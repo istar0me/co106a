444807e1c36a3b04c886ca551aea353aebe95cc4
Alan <istar0me@gmail.com>
2017-11-24 18:36:35 +0800

    add fill, mult.asm

--- exercise/04/fill/Fill.asm
+++ exercise/04/fill/Fill.asm
@@ -11,4 +11,71 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Put your code here
+// forever() {
+(forever)
+
+// scrAddr = RAM[SCREEN]
+@SCREEN
+D=A
+@scrAddr
+M=D
+
+// n = 8192
+@8192
+D=A
+@n
+M=D
+
+// i = 0
+@0
+D=A
+@i
+M=D
+
+// function loop() {
+// if((n-i) == 0) {goto end}
+@i
+D=M
+@n
+D=M-D
+@end
+D;JEQ
+
+// if (KBD == 0) {else()}
+@KBD
+D=M
+@else
+D;JEQ
+
+// if (KBD != 0 ) {RAM[scrAddr + i] = -1}
+@scrAddr
+D=M
+@i
+A=D+M
+M=-1
+
+// endif()
+@endif
+0;JEQ
+
+// function else()
+(else)
+@scrAddr
+D=M
+@i
+A=D+M
+M=0
+
+// function endif()
+@i
+M=M+1
+
+// loop()
+@loop
+0;JEQ
+
+// function end() {goto forever}
+(end)
+@forever
+0;JEQ
--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// int R0 = 3;
+@3
+D=A
+@R0
+M=D
+
+// int R1 = 5;
+@5
+D=A
+@R1
+M=D
+
+// int R3 = 0;
+@0
+D=A
+@sum
+M=D
+
+// loop:
+(loop)
+
+// if (R0 <= 0) goto exit1;
+@R0
+D=A
+@exit1
+D;JLE
+
+// R2 = R2 + R1;
+@R1
+D=M
+@R2
+M=M+D
+
+// r0 = r0 - 1;
+@r0
+M=M-1
+
+// goto loop;
+@loop
+0;JMP
+
+// exit1:
+(exit1)
+@exit1
+0;JMP
